59fbcd7bbb68e6b681a6be9702824834b0ce87c4
Benoit Denkinger <benoit.denkinger@cern.ch>
2024-11-07 13:44:21 +0100

    removed tech_cells_generic package fetching and ip linking.

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -17,9 +17,13 @@ set(CPM_DOWNLOAD_VERSION 0.38.1)
 # Include the CPM.cmake downloader
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
-CPMAddPackage(
-    NAME tech_cells_generic
-    # Commit on socmake branch
-    GIT_TAG b4d2371
-    GIT_REPOSITORY "https://github.com/HEP-SoC/tech_cells_generic.git"
-)
+# For now, all the cells we use DO NOT make use of tech_cells_generic so
+# the package is removed to avoid using its cells transparently
+# If this is ever the case, consider using prim_cells instead
+
+# CPMAddPackage(
+#     NAME tech_cells_generic
+#     # Commit on socmake branch
+#     GIT_TAG b4d2371
+#     GIT_REPOSITORY "https://github.com/HEP-SoC/tech_cells_generic.git"
+# )
